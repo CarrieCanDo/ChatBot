2aa752b735ca013a387e433b212ac5a710ff75bd
CarrieCanDo <carrie.a.wright@outlook.com>
2024-07-25 09:30:29 -0400

    committing files

--- src/main/java/org/example/ChatBot.java
+++ src/main/java/org/example/ChatBot.java
@@ -26,63 +26,26 @@ public class ChatBot {
         System.out.println("Thank you, " + repeatedName + "!");
     }
 
-
     public void guessAge() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Let's play a game. I'll guess your age by asking a few questions.");
 
-        System.out.println("Are you older than 20? (yes/no)");
-        String response = scanner.nextLine();
-        boolean olderThan20 = response.equalsIgnoreCase("yes");
+        int lowerBound = 0;
+        int upperBound = 100;
 
-        System.out.println("Are you older than 30? (yes/no)");
-        response = scanner.nextLine();
-        boolean olderThan30 = response.equalsIgnoreCase("yes");
+        while (lowerBound < upperBound) {
+            int midPoint = (lowerBound + upperBound) / 2;
+            System.out.println("Are you older than " + midPoint + "? (yes/no)");
+            String response = scanner.nextLine();
 
-        System.out.println("Are you older than 40? (yes/no)");
-        response = scanner.nextLine();
-        boolean olderThan40 = response.equalsIgnoreCase("yes");
-
-        System.out.println("Are you older than 50? (yes/no)");
-        response = scanner.nextLine();
-        boolean olderThan50 = response.equalsIgnoreCase("yes");
-
-        System.out.println("Are you older than 60? (yes/no)");
-        response = scanner.nextLine();
-        boolean olderThan60 = response.equalsIgnoreCase("yes");
-
-        int age = 0;
-        if (olderThan60) {
-            System.out.println("Are you older than 70? (yes/no)");
-            response = scanner.nextLine();
-            boolean olderThan70 = response.equalsIgnoreCase("yes");
-
-            if (olderThan70) {
-                System.out.println("Are you older than 80? (yes/no)");
-                response = scanner.nextLine();
-                boolean olderThan80 = response.equalsIgnoreCase("yes");
-
-                age = olderThan80 ? 80 : 70;
+            if (response.equalsIgnoreCase("yes")) {
+                lowerBound = midPoint + 1;
             } else {
-                age = 60;
+                upperBound = midPoint;
             }
-        } else if (olderThan50) {
-            age = 50;
-        } else if (olderThan40) {
-            age = 40;
-        } else if (olderThan30) {
-            age = 30;
-        } else if (olderThan20) {
-            age = 20;
-        } else {
-            System.out.println("Are you younger than 10? (yes/no)");
-            response = scanner.nextLine();
-            boolean youngerThan10 = response.equalsIgnoreCase("yes");
-
-            age = youngerThan10 ? 5 : 15;
         }
 
-        System.out.println("I guess your age is around " + age + " years old!");
+        System.out.println("I guess your age is " + lowerBound + " years old!");
     }
 
     public void countToNumber() {
